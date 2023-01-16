1ffdc9c900a6546d56b0b17051655cf840ed7f00
minh97 <minhnguyen@beeketing.net>
2023-01-16 12:46:11 +0700

    update ssr trip

--- layouts/trip.vue
+++ layouts/trip.vue
@@ -20,6 +20,11 @@ export default {
       textNav: 'white--text'
     }
   },
+  head: {
+    script: [
+      { hid: 'woay', src: 'https://woay.space/w/4cac5298-4693-4ccc-9c95-ee60e19a057b/popup.js', defer: true }
+    ]
+  },
   components: {
     Header: () => import("@/layouts/sections/Header"),
     Footer: () => import("@/layouts/sections/Footer")
